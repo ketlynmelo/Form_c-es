2b8f05bda9ef9a8d54ff1826259a0ed763c3fd7b
ketlynmelo <ketlyn_de-melo@estudante.sesisenai.org.br>
2026-03-27 12:42:12 -0300

    ADD - Complementei o trabalho com mais informacoes

--- script.js
+++ script.js
@@ -10,11 +10,27 @@ let form = document.getElementById('formulario');
         let email = document.getElementById('email').value;
         let telefone = document.getElementById('telefone').value;
         let cpf = document.getElementById('cpf').value;
+        let idade = document.getElementById('idade').value;
+        let cidade = document.getElementById('cidade').value;
+        let moradia = document.getElementById('moradia').value;
+        let quintal = document.getElementById('quintal').value;
+        let horas = document.getElementById('horas').value;
+        let motivo = document.getElementById('motivo').value;
+        let termos = document.getElementById('Termos').value;
+
+
 
         document.getElementById('erroNome').textContent = '';
         document.getElementById('erroEmail').textContent = '';
         document.getElementById('erroTelefone').textContent = '';
         document.getElementById('erroCPF').textContent = '';
+        document.getElementById('erroIdade').textContent = '';
+        document.getElementById('erroCidade').textContent = '';
+        document.getElementById('erroMoradia').textContent = '';
+        document.getElementById('erroQuintal').textContent = '';
+        document.getElementById('erroHoras').textContent = '';
+        document.getElementById('erroMotivo').textContent = '';
+        document.getElementById('erroTermos').textContent = '';
 
         if (nome.length < 3) {
 
@@ -28,10 +44,44 @@ let form = document.getElementById('formulario');
             valido = false;
         } 
 
-        if(senha.length < 6){
+        if(telefone.length < 8){
             
-            document.getElementById('erroSenha').textContent = 'Senha inválida, deve ter pelo menos 6 caracteres';
+            document.getElementById('erroTelefone').textContent = 'Telefone inválido, deve ter pelo menos 8 caracteres';
             valido = false;
+        }
+
+        if(idade.length < 18){
+            
+            document.getElementById('erroIdade').textContent = 'Idade insuficiente, deve ser pelo menos 18 anos ';
+            valido = false;
+        }
+
+        if(moradia= Apartamento){
+           let local = prompt("O seu local aceita animais?");
+           if (local=="Não || não || Nao || Nao"){
+            
+
+           }
+
+
+        }
+
+        if(quintal.length ){
+            document.getElementById('erroQuintal').textContent = 'Resposta incorreta, deve falar sim ou não!';
+            valido = false;
+
+        }
+
+        if(pet.length ){
+            document.getElementById('erroPet').textContent = 'Resposta incorreta, deve falar sim ou não!';
+            valido = false;
+
+        }
+
+        if(motivo.length < 10){
+            document.getElementById('erroIdade').textContent = 'Sua explicação deve ter no minimo 10 caracteres';
+            valido = false;
+
         }
 
 
@@ -43,7 +93,15 @@ let form = document.getElementById('formulario');
             Dados enviados: <br>
             Nome: ${nome} <br>
             Email: ${email} <br>
-            Senha: ${senha} <br>
+            Telefone: ${telefone} <br>
+            CPF: ${cpf} <br>
+            Idade: ${idade} <br>
+            Cidade: ${cidade} <br>
+            Tipo de moradia: ${moradia} <br>
+            Possui quintal: ${quintal} <br>
+            Já teve pet antes: ${pet} <br>
+            Quantas horas o animal ficará sozinho por dia: ${horas} <br>
+            Motivo da adoção: ${motivo} <br>
 
             `;
 
